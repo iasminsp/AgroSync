806eef9db9f69b917703874628b5f83c58518f11
iasminsp <130411997+iasminsp@users.noreply.github.com>
2024-09-04 02:44:40 -0300

    telas separadas e organizaçao do layout

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,33 +1,76 @@
-import { Link, Tabs } from "expo-router";
-import { HeaderButton } from "../../components/HeaderButton";
-import { TabBarIcon } from "../../components/TabBarIcon";
+import { Tabs } from "expo-router";
+import { MaterialIcons } from "@expo/vector-icons";
+import { useState, useContext } from "react";
 
 export default function TabLayout() {
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: "black",
-      }}
-    >
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: "Tab One",
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-          headerRight: () => (
-            <Link href="/modal" asChild>
-              <HeaderButton />
-            </Link>
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="two"
-        options={{
-          title: "Tab Two",
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
+  const [route, setRoute] = useState('menu');
+    return (
+        <Tabs screenOptions={
+            {
+                headerShown: false,
+                tabBarStyle: {
+                    backgroundColor: '#D9D9D9',
+                    borderTopWidth: 0,
+                    minHeight: 74,
+                },
+                tabBarItemStyle: {
+                    paddingBottom: 25,
+                    paddingTop: 14,
+                },
+                tabBarShowLabel: false,
+                tabBarActiveTintColor: '#FFFF00',
+                tabBarInactiveTintColor: '#D9D9D9',
+            }
+        }>
+            <Tabs.Screen listeners={{
+                tabPress: (e) => { setRoute('menu'); },
+            }}
+                name="menu"
+                options={{
+                    tabBarIcon: () => <MaterialIcons name="home" size={30} color="#1E4034"
+                        style={{
+                            borderRadius: 200,
+                            backgroundColor: route === 'menu' ? '#FFFFFF' : '#FFFFFF00',
+                        }} />
+                }} />
+
+
+            <Tabs.Screen listeners={{
+                tabPress: (e) => { setRoute('saude'); },
+            }}
+                name="saude"
+                options={{
+                    tabBarIcon: () => <MaterialIcons name="circle" size={30} color="#1E4034"
+                        style={{
+                            borderRadius: 200,
+                            backgroundColor: route === 'saude' ? '#FFFFFF' : '#FFFFFF00',
+                        }} />
+                }} />
+
+            <Tabs.Screen listeners={{
+                tabPress: (e) => { setRoute('despesasFormulario'); },
+            }}
+                name="despesasFormulario"
+                options={{
+                    tabBarIcon: () => <MaterialIcons name="money" size={30} color="#1E4034"
+                        style={{
+                            borderRadius: 200,
+                            backgroundColor: route === 'despesasFormulario' ? '#FFFFFF' : '#FFFFFF00',
+                        }} />
+                }} />
+
+            <Tabs.Screen listeners={{
+                tabPress: (e) => { setRoute('relatorio'); },
+            }}
+                name="relatorio"
+                options={{
+                    tabBarIcon: () => <MaterialIcons name="add-task" size={30} color="#1E4034"
+                        style={{
+                            borderRadius: 200,
+                            backgroundColor: route === 'relatorio' ? '#FFFFFF' : '#FFFFFF00',
+                        }} />
+                }} />
+        </Tabs>
+    )
+
 }
