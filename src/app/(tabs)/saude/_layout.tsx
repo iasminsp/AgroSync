5bfdc45f91e8b047d9fc83ed753caeea565bb1c2
iasminsp <130411997+iasminsp@users.noreply.github.com>
2024-09-19 00:24:34 -0300

    botão da area de saude

--- src/app/(tabs)/saude/_layout.tsx
+++ src/app/(tabs)/saude/_layout.tsx
@@ -1,7 +1,6 @@
 import { Stack } from "expo-router";
 
 export default function saudeLayout() {
-    
     return    (
         <Stack screenOptions={{headerShown: false,}}>
             <Stack.Screen name="index" />
