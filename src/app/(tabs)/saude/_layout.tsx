3d72638ca863dfef4d81df53e4cd8491f4886ea2
iasminsp <130411997+iasminsp@users.noreply.github.com>
2024-09-20 22:06:17 -0300

    card e telas novas da area saude feito

--- src/app/(tabs)/saude/_layout.tsx
+++ src/app/(tabs)/saude/_layout.tsx
@@ -1,9 +1,12 @@
 import { Stack } from "expo-router";
 
+
 export default function saudeLayout() {
     return    (
         <Stack screenOptions={{headerShown: false,}}>
             <Stack.Screen name="index" />
+            <Stack.Screen name="Previsao" />
+            <Stack.Screen name="Doentes" />
         </Stack>
     )
 }
