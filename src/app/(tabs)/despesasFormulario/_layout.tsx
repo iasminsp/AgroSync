a5e1e36433c49e933d465392af7107c80734f6eb
laysgabrielle <130411960+laysgabrielle@users.noreply.github.com>
2024-10-12 15:48:19 -0300

    telas relatorio

--- src/app/(tabs)/despesasFormulario/_layout.tsx
+++ src/app/(tabs)/despesasFormulario/_layout.tsx
@@ -1,3 +1,15 @@
-export default function despesasFormularioLayout() {
 
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { Stack } from "expo-router";
+
+
+export default function despesasFormularioLayout() {
+    return    (
+        <Stack screenOptions={{headerShown: false,}}>
+            <Stack.Screen name="index" />
+            <Stack.Screen name="Previsao" />
+            <Stack.Screen name="Doentes" />
+        </Stack>
+    )
 }
--- src/app/(tabs)/despesasFormulario/index.tsx
+++ src/app/(tabs)/despesasFormulario/index.tsx
@@ -1,3 +1,17 @@
-export default function despesasFormulario() {
+import { View, Text , Image} from "react-native";
+import Formulario from "src/app/components/FormularioMensal";
+import { useNavigation } from '@react-navigation/native';
 
+
+export default function index() {
+    return (
+        <View 
+         >
+
+            
+            <View>
+            <Formulario titulo={"formularios"} rota={"telaFormulario"}/>
+            </View>
+        </View>    
+    )
 }
