6e1b65dfcfccef5de5a49abe4a37507b2fa773bc
iasminsp <130411997+iasminsp@users.noreply.github.com>
2024-11-26 16:35:43 -0300

    ultimos ajustes saude

--- src/components/saude/superiorSaude.tsx
+++ src/components/saude/superiorSaude.tsx
@@ -12,7 +12,7 @@ const SuperiorSaude: React.FC<SuperiorProps> = ({ titulo }) => {
         position: 'relative',
         backgroundColor: '#1E4034',
         width: '100%',
-        height: '10%', // Ajuste de altura para porcentagem
+        height: '15%', // Ajuste de altura para porcentagem
         // shadowColor: '#000',
         // shadowOffset: { width: 0, height: 2 },
         // shadowOpacity: 0.25,
@@ -32,7 +32,7 @@ const SuperiorSaude: React.FC<SuperiorProps> = ({ titulo }) => {
           backgroundColor: '#d7d7d7',
         }}
       />
-      <View style={{ position: 'absolute', left: '10%', top: '5%' }}>
+      <View style={{ position: 'absolute', left: '10%', top: '40%' }}>
         <Text style={{ color: 'white', fontSize: 22, fontStyle: 'italic' }}>
           {titulo}
         </Text>
